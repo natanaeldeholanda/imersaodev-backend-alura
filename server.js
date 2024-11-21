35334713c6e90808f2c5622326c0b5c32b5fb95e
Natanael de Holanda e Oliveira <natan.holanda@gmail.com>
2024-11-21 16:41:55 -0300

    Refatorado, mas ainda sem conexão. Rever aula 03

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 import express from "express";
 import conectarAoBanco from "./src/config/dbConfig.js";
-
-await conectarAoBanco(process.env.STRING_CONEXAO)
+const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
 
 const posts = [
   { id: 1, descricao: "Uma foto test", imagem: "https://placecats.com/millie/300/150"},
@@ -12,16 +11,23 @@ const app = express();
 app.use(express.json());
 app.listen(3000, () => {
   console.log("Servidor escutando na porta 3000");
-});a
+});
 
 // Função para buscar um post por id
-function buscarPostPorId(id) {
-  return posts.findIndex((post) =>{
-    return post.id === Number(id);
-  });q
+// function buscarPostPorId(id) {
+//   return posts.findIndex((post) =>{
+//     return post.id === Number(id);
+//   });
+// }
+async function getTodosPosts(req, res) {
+  const db = conexao.db("ImersaoDevBackendAlura");
+  const colecao = db.collection("posts");
+  return colecao.find().toArray();
 }
-app.get("/posts", (req, res) => {
-    res.status(200).json(posts);
+
+app.get("/posts", async(req, res) => {
+  const resultado = await getTodosPosts(req, res);
+    res.status(200).json(resultado);
 });
 app.get("/posts/:id", (req, res) => {
   const index = buscarPostPorId(req.params.id);
